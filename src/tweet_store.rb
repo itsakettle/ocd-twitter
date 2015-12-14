ba166c8ce76162a5f45ad95688119609d5da0e83
wtrp <you@example.com>
2015-12-14 21:37:24 +0000

    dynamo config handled in yml file

--- src/tweet_store.rb
+++ src/tweet_store.rb
@@ -4,21 +4,23 @@ module OcdTweets
   
   class TweetStore
   
-    TABLE = 'ocd_tweets_dev'
-  
     def self.client 
       @client ||= Aws::DynamoDB::Client.new(region: "eu-west-1")
+    end
+    
+    def self.table
+      @table ||= YAML.load_file('./config/dynamo.yml')['dynamo_table']
     end
   
     def self.write_tweet(user_id:, timestamp:, small_tweet:)
       item = {user_id: user_id, timestamp: timestamp, small_tweet: small_tweet}
-      client.put_item(table_name: TABLE, item: item)
+      client.put_item(table_name: table, item: item)
     end
     
     def self.get_tweets(start_key: ,projection_expression:)
 
       client.scan(projection_expression: projection_expression,
-                  table_name: TABLE,
+                  table_name: table,
                   exclusive_start_key: start_key,
                   expression_attribute_names: {"#ts" => "timestamp"})
 
@@ -26,7 +28,7 @@ module OcdTweets
     
     def self.update(user_id:, timestamp:, attribute:)
       key = {user_id: user_id, timestamp: timestamp}
-      client.update_item(table_name: TABLE, key: key, attribute_updates: attribute )
+      client.update_item(table_name: table, key: key, attribute_updates: attribute )
     end
   
   end
